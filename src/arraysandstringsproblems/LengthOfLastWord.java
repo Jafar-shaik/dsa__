cc065485e696f6ef8965b97e37cdc5fe6427eb1f
Jafar-shaik <skjafar4944@gmail.com>
2026-02-20 10:47:55 +0530

    Modified Length of last word

--- src/arraysandstringsproblems/LengthOfLastWord.java
+++ src/arraysandstringsproblems/LengthOfLastWord.java
@@ -3,17 +3,18 @@ package arraysandstringsproblems;
 public class LengthOfLastWord {
     public static void main(String[] args) {
 
-        String sentence = "jafar is a very good boy   ";
+        String sentence = "  Jafar is good boyyyy    ";
         int count = 0, n = sentence.length();
-
-        for(int i = n-1; i >= 0; i--){
-            char ch = sentence.charAt(i);
-            if(ch != ' '){
-                count++;
-            }else if(ch > 0){
-                break;
-            }
+        int index = n-1;
+        while(index >= 0 && sentence.charAt(index) == ' '){
+            index--;
         }
+
+        while(index >= 0 && sentence.charAt(index) != ' '){
+            index--;
+            count++;
+        }
+
         System.out.println(count);
     }
 }
